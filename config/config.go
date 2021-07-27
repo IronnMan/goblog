67496c03726c3769f6272b9e1746a3c9ab6ab9a6
wenlin42 <wenlin42@icloud.com>
2021-07-28 03:40:52 +0800

    配置和环境变量

---
+++ config/config.go
@@ -0,0 +1,6 @@
+package config
+
+// Initialize 配置信息初始化
+func Initialize() {
+	// 触发加载目录下其它文件中的 init 方法
+}
--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
+	"goblog/config"
+	c "goblog/pkg/config"
 	"net/http"
 )
+
+func init()  {
+	// 初始化配置信息
+	config.Initialize()
+}
 
 func main() {
 
 	bootstrap.SetupDB()
 	router := bootstrap.SetupRoute()
 
-	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	http.ListenAndServe(":" + c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
 }
