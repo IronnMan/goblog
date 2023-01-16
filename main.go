cf9a3858d5f97b03f49e314026f9006c38a9aa11
wenlin42 <wenlin42@icloud.com>
2023-01-16 21:52:51 +0800

    add about page

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, Here it is goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, Here it is goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "<h1>About me</h1>")
+	} else {
+		fmt.Fprint(w, "<h1>404</h1>")
+	}
 }
 
 func main() {
