356bf68d1c5ee42b7121b1c0862da82a8cfb687e
wenlin42 <wenlin42@icloud.com>
2023-01-17 00:28:11 +0800

    设置标头

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, Here it is goblog!</h1>")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<h1>About me</h1>")
+		fmt.Fprint(w, "About me")
 	} else {
 		fmt.Fprint(w, "<h1>404</h1>")
 	}
