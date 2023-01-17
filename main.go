cd8ac3dd9047f1546956ca2729900daf7e7aa627
wenlin42 <wenlin42@icloud.com>
2023-01-18 01:13:03 +0800

    Try http.ServeMux

--- main.go
+++ main.go
@@ -3,21 +3,45 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, Here it is goblog!</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "About me")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>404</h1>")
 	}
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "About me")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+
+	// 文章详情
+	router.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		id := strings.SplitN(r.URL.Path, "/", 3)[2]
+		fmt.Fprint(w, "Article ID: "+id)
+	})
+
+	// 列表 or 创建
+	router.HandleFunc("/articles", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			fmt.Fprint(w, "Access article list")
+		case "POST":
+			fmt.Fprint(w, "Create new article")
+		}
+	})
+
+	http.ListenAndServe(":3000", router)
 }
