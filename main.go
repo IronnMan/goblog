fbf4b65243a27dff1a937b24aa042551a128f2d8
wenlin42 <wenlin42@icloud.com>
2023-01-19 00:00:57 +0800

    use http middleware

--- main.go
+++ main.go
@@ -7,18 +7,15 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Hello, Here it is goblog!</h1>")
 
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "About me")
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "404 Page not found")
 }
@@ -37,6 +34,15 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Create new article")
 }
 
+func forceHTMLMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1. 设置标头
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		// 2. 继续处理请求
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	router := mux.NewRouter()
 
@@ -49,6 +55,9 @@ func main() {
 
 	// 自定义 404 页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	// 中间件：强制内容类型为 HTML
+	router.Use(forceHTMLMiddleware)
 
 	// 通过命名路由获取 URL 示例
 	homeURL, _ := router.Get("home").URL()
