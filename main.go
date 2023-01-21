883f0f752a33ccf15770a34475a669d3edc74f67
wenlin42 <wenlin42@icloud.com>
2023-01-21 16:49:33 +0800

    Article list

--- main.go
+++ main.go
@@ -37,6 +37,16 @@ type Article struct {
 	ID          int64
 }
 
+// Link 方法用来生成文章链接
+func (a Article) Link() string {
+	showURL, err := router.Get("articles.show").URL("id", strconv.FormatInt(a.ID, 10))
+	if err != nil {
+		checkError(err)
+		return ""
+	}
+	return showURL.String()
+}
+
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取 URL 参数
 	id := getRouteVariable("id", r)
@@ -67,7 +77,32 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Access article list")
+	// 1. 执行查询语句，返回一个结果集
+	rows, err := db.Query("SELECT * FROM articles")
+	checkError(err)
+	defer rows.Close()
+
+	var articles []Article
+	// 2. 循环读取结果
+	for rows.Next() {
+		var article Article
+		// 扫描每一行的结果并赋值到一个 article 对象中
+		err := rows.Scan(&article.ID, &article.Title, &article.Body)
+		checkError(err)
+		// 将 article 追加到 articles 的这个数组中
+		articles = append(articles, article)
+	}
+	// 检测遍历时是否发生错误
+	err = rows.Err()
+	checkError(err)
+
+	// 3. 加载模版
+	tmpl, err := template.ParseFiles("resources/views/articles/index.gohtml")
+	checkError(err)
+
+	// 4. 渲染模版，将所有文章的数据传输进去
+	err = tmpl.Execute(w, articles)
+	checkError(err)
 }
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
