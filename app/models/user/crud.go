9a24ae2336967f111df1c87548e8a4c7d8686cbb
wenlin42 <wenlin42@icloud.com>
2023-02-07 02:43:50 +0800

    Create user

---
+++ app/models/user/crud.go
@@ -0,0 +1,15 @@
+package user
+
+import (
+	"goblog/pkg/logger"
+	"goblog/pkg/model"
+)
+
+func (u *User) Create() (err error) {
+	if err = model.DB.Create(&u).Error; err != nil {
+		logger.LogError(err)
+		return err
+	}
+
+	return nil
+}
