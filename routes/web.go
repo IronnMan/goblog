f52591d6f1baf6217a380e85907b521a065b990c
wenlin42 <wenlin42@icloud.com>
2023-02-07 15:21:37 +0800

    Auth login & session

--- routes/web.go
+++ routes/web.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gorilla/mux"
 	"goblog/app/http/controllers"
+	"goblog/app/http/middlewares"
 	"net/http"
 )
 
@@ -11,8 +12,10 @@ func RegisterWebRoutes(r *mux.Router) {
 	// 静态资源
 	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
 	r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
-	// 中间件：强制内容类型为 HTML
+	// 全局中间件
 	//r.Use(middlewares.ForceHTML)
+	// 开始会话
+	r.Use(middlewares.StartSession)
 	// 静态页面
 	pc := new(controllers.PagesController)
 	r.NotFoundHandler = http.HandlerFunc(pc.NotFound)
@@ -31,4 +34,6 @@ func RegisterWebRoutes(r *mux.Router) {
 	auc := new(controllers.AuthController)
 	r.HandleFunc("/auth/register", auc.Register).Methods("GET").Name("auth.register")
 	r.HandleFunc("/auth/do-register", auc.DoRegister).Methods("POST").Name("auth.doregister")
+	r.HandleFunc("/auth/login", auc.Login).Methods("GET").Name("auth.login")
+	r.HandleFunc("/auth/dologin", auc.DoLogin).Methods("POST").Name("auth.dologin")
 }
