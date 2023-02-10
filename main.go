7ab96b9a347e7f38f4f5dadc60099e36ba2cf35f
wenlin42 <wenlin42@icloud.com>
2023-02-10 11:30:42 +0800

    embed packaging template file

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"embed"
 	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
 	"goblog/config"
 	c "goblog/pkg/config"
 	"net/http"
 )
+
+//go:embed resources/views/articles/*
+//go:embed resources/views/auth/*
+//go:embed resources/views/categories/*
+//go:embed resources/views/layouts/*
+var tplFS embed.FS
 
 func init() {
 	// 初始化配置信息
@@ -16,6 +23,9 @@ func init() {
 func main() {
 	// 初始化 SQL
 	bootstrap.SetupDB()
+
+	// 初始化模版
+	bootstrap.SetupTemplate(tplFS)
 
 	// 初始化路由绑定
 	router := bootstrap.SetupRoute()
